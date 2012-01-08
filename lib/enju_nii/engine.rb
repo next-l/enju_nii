bb83ce7b00f8f3373ba1dd32724a70e604da71a6
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2012-01-09 02:28:32 +0900

    added locale files

--- lib/enju_nii/engine.rb
+++ lib/enju_nii/engine.rb
@@ -1,3 +1,7 @@
+require 'devise'
+require 'cancan'
+require 'inherited_resources'
+
 module EnjuNii
   class Engine < Rails::Engine
   end
---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '432c5ea92019f8e8027800416a26c0b21dce8172c71e7132dc93dbdde8a389e3f4e76d70ad9ce9b5c6cb97293732f672072872c785ff5dfedbf1ac90efbaee8b'
