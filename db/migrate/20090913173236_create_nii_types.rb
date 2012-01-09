081a0464b9159a50463fc7d27930f96fcc739f53
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2012-01-09 19:20:49 +0900

    added migration files

--- db/migrate/20090913173236_create_nii_types.rb
+++ db/migrate/20090913173236_create_nii_types.rb
@@ -2,11 +2,12 @@ class CreateNiiTypes < ActiveRecord::Migration
   def change
     create_table :nii_types do |t|
       t.string :name, :null => false
-      t.string :display_name
+      t.text :display_name
       t.text :note
       t.integer :position
 
       t.timestamps
     end
+    add_index :nii_types, :name, :unique => true
   end
 end
