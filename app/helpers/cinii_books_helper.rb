bae5ad110df44ab5d02112158a750b738862d7bd
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2012-11-14 20:27:34 +0900

    added locale files

---
+++ app/helpers/cinii_books_helper.rb
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+module CiniiBooksHelper
+  def link_to_import(ncid)
+    if ncid.blank?
+      t('enju_nii.not_available')
+    else
+      manifestation = Manifestation.where(:ncid => ncid).first
+      unless manifestation
+        link_to t('enju_nii.add'), cinii_books_path(:book => {:ncid => ncid}), :method => :post
+      else
+        link_to t('enju_nii.already_exists'), manifestation
+      end
+    end
+  end
+end
