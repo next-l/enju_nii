73aa15407461753ee3fb13721b9f0a4fd61c86c0
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2013-04-12 02:17:22 +0900

    updated generator

--- lib/generators/enju_nii/setup/setup_generator.rb
+++ lib/generators/enju_nii/setup/setup_generator.rb
@@ -4,9 +4,5 @@ class EnjuNii::SetupGenerator < Rails::Generators::Base
   def copy_setup_files
     directory("db/fixtures", "db/fixtures/enju_nii")
     rake("enju_nii_engine:install:migrations")
-    inject_into_class 'app/models/manifestation.rb', Manifestation,
-      "  enju_nii_cinii_books\n"
-    inject_into_class 'app/models/import_request.rb', ImportRequest,
-      "  enju_nii_cinii_books\n"
   end
 end
