8ae86a20360888b102cebd9c92db29903500b0c5
Kosuke Tanabe <nabeta@fastmail.fm>
2018-02-17 22:08:32 +0900

    update casette files

--- spec/models/cinii_book_spec.rb
+++ spec/models/cinii_book_spec.rb
@@ -6,7 +6,7 @@ describe CiniiBook do
   Manifestation.enju_nii_cinii_books
 
   it 'should search bibliographic records', vcr: true do
-    CiniiBook.search('library system')[:total_entries].should eq 3563
+    CiniiBook.search('library system')[:total_entries].should eq 3579
   end
 
   it 'should search with ncid', vcr: true do
