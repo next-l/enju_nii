11ae6aa5bfdadfe084dddc02a7381202f4a828cf
Kosuke Tanabe <nabeta@fastmail.fm>
2019-01-13 16:10:46 +0900

    fix index definition

--- app/models/concerns/enju_nii/enju_manifestation.rb
+++ app/models/concerns/enju_nii/enju_manifestation.rb
@@ -5,8 +5,10 @@ module EnjuNii
     included do
       belongs_to :nii_type
       has_one :ncid_record
-      string :ncid do
-        ncid_record.try(:body)
+      searchable do
+        string :ncid do
+          ncid_record.try(:body)
+        end
       end
     end
   end
