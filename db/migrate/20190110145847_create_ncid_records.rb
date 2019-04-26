3ad23f6a3fa38c0fe8dc097678e57ebe5584e220
Kosuke Tanabe <nabeta@fastmail.fm>
2019-04-26 11:38:02 +0900

    revert using uuid

--- db/migrate/20190110145847_create_ncid_records.rb
+++ db/migrate/20190110145847_create_ncid_records.rb
@@ -2,7 +2,7 @@ class CreateNcidRecords < ActiveRecord::Migration[5.2]
   def change
     create_table :ncid_records do |t|
       t.string :body, null: false, index: {unique: true}
-      t.references :manifestation, foreign_key: true, null: false, type: :uuid
+      t.references :manifestation, foreign_key: true, null: false
 
       t.timestamps
     end
